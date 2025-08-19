9d21ae743f8d2ae2f37dd905555679a81649a809
David-396 <david.eini12@gmail.com>
2025-08-19 18:17:33 +0300

    create the yaml files and apply commands

---
+++ scripts/commands.bat
@@ -0,0 +1,8 @@
+oc apply -f ..\infrastructure\k8s\mongoDB-secret.yaml
+oc apply -f ..\infrastructure\k8s\mongoDB-deployment.yaml
+oc apply -f ..\infrastructure\k8s\mongoDB-svc.yaml
+oc apply -f ..\infrastructure\k8s\crud-app-secret.yaml
+oc apply -f ..\infrastructure\k8s\crud-app-deployment.yaml
+oc apply -f ..\infrastructure\k8s\crud-app-svc.yaml
+
+oc expose svc/mongodb-crud-svc
